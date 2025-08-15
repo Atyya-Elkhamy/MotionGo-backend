d8b167c478c5a3d7fcb63fd84c53f8490e6aac5e
Atyya <atiaelkhamy55@gmail.com>
2025-08-15 16:19:55 +0300

    update validateion ;

--- project/accounts/serializers.py
+++ project/accounts/serializers.py
@@ -11,19 +11,19 @@ class UserSerializer(serializers.ModelSerializer):
         email = attrs["email"]
         password = attrs["password"]
         phone = attrs["phone"]
-        username_regex = r'^[a-zA-Z_ ]+$'
-        if not re.match(username_regex, username):
-            raise serializers.ValidationError({"username": "Only letters, underscores, and spaces are allowed"})
-        username = username.strip()
+        # username_regex = r'^[a-zA-Z_ ]+$'
+        # if not re.match(username_regex, username):
+        #     raise serializers.ValidationError({"username": "Only letters, underscores, and spaces are allowed"})
+        # username = username.strip()
         email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
         if not re.match(email_regex, email):
             raise serializers.ValidationError({"email":"Invalid email format"})
         password_regex = r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$'
         if not re.match(password_regex, password):
             raise serializers.ValidationError({"password":"Password must be at least 8 characters long and include both letters and numbers"})
-        phone_regex = r'^(010|012|015|011)\d{8}$'
-        if not re.match(phone_regex, phone):
-            raise serializers.ValidationError({"phone":"Phone number must be 11 digits and start with 010, 012, 015, or 011"})
+        # phone_regex = r'^(010|012|015|011)\d{8}$'
+        # if not re.match(phone_regex, phone):
+        #     raise serializers.ValidationError({"phone":"Phone number must be 11 digits and start with 010, 012, 015, or 011"})
         return attrs
     
     def create(self, validated_data):
